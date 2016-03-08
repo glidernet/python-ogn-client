7a7fcd3a67b640048b4e492d663d345140050cd4
Fabian P. Schmidt <kerel-fs@gmx.de>
2016-03-08 02:14:18 +0100

    parser: Import parse functions at package level
    Simplify the import of parse functions (internally distributed in
    submodules).

---
+++ ogn/parser/__init__.py
@@ -0,0 +1,2 @@
+from ogn.parser.parse import parse_aprs, parse_ogn_beacon, parse_ogn_receiver_beacon, parse_ogn_aircraft_beacon # flake8: noqa
+from ogn.parser.exceptions import ParseError, AprsParseError, OgnParseError, AmbigousTimeError # flake8: noqa
