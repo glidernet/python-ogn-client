bc3e31838fd1b5eec3c4ca17f5fab4588ac87904
Konstantin Gründger <kammermark@gmx.de>
2017-04-03 08:08:03 +0200

    Update to receiver version 0.2.6
    Update to receiver version 0.2.6
    
    Better testing

--- tests/client/test_TelnetClient.py
+++ tests/client/test_TelnetClient.py
@@ -6,9 +6,21 @@ from ogn.client.client import TelnetClient
 
 class TelnetClientTest(unittest.TestCase):
     @mock.patch('ogn.client.client.socket')
-    def test_connect(self, socket_mock):
+    def test_connect_disconnect(self, socket_mock):
+        client = TelnetClient()
+        client.connect()
+        client.sock.connect.assert_called_once()
+
+        client.disconnect()
+        client.sock.shutdown.assert_called_once()
+        client.sock.close.assert_called_once()
+
+    @mock.patch('ogn.client.client.socket')
+    def test_run(self, socket_mock):
         def callback(raw_message):
-            pass
+            raise ConnectionRefusedError
 
         client = TelnetClient()
+        client.connect()
+
         client.run(callback=callback)
