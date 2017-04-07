d40e4c797e7cab9d15e521fb3acbbda29bde473b
kammermark@gmx.de <kammermark@gmx.de>
2017-04-07 15:53:35 +0200

    Cleaned pattern

--- ogn/parser/pattern.py
+++ ogn/parser/pattern.py
@@ -30,18 +30,19 @@ PATTERN_APRS_STATUS = re.compile(r"(?P<callsign>.+?)>(?P<dstcall>[A-Z0-9]+),.+,(
 # THE SOFTWARE.
 
 PATTERN_RECEIVER_BEACON = re.compile(r"""
+    ^
     (?:
         v(?P<version>\d+\.\d+\.\d+)
         \.?(?P<platform>.+?)?
     \s)?
-    CPU:(?P<cpu_load>[\d.]+)\s
-    RAM:(?P<ram_free>[\d.]+)\/(?P<ram_total>[\d.]+)MB\s
-    NTP:(?P<ntp_offset>[\d.]+)ms\/(?P<ntp_correction>[+-][\d.]+)ppm\s
-    (?:(?P<voltage>[\d.]+)V\s)?
-    (?:(?P<amperage>[\d.]+)A\s)?
-    (?:(?P<cpu_temperature>[+-][\d.]+)C\s*)?
-    (?:(?P<visible_senders>\d+)\/(?P<senders>\d+)Acfts\[1h\]\s*)?
-    (?:RF:
+    (?:\s?CPU:(?P<cpu_load>[\d.]+))?
+    (?:\sRAM:(?P<ram_free>[\d.]+)\/(?P<ram_total>[\d.]+)MB)?
+    (?:\sNTP:(?P<ntp_offset>[\d.]+)ms\/(?P<ntp_correction>[+-][\d.]+)ppm)?
+    (?:\s(?P<voltage>[\d.]+)V)?
+    (?:\s(?P<amperage>[\d.]+)A)?
+    (?:\s(?P<cpu_temperature>[+-][\d.]+)C)?
+    (?:\s(?P<visible_senders>\d+)\/(?P<senders>\d+)Acfts\[1h\])?
+    (?:\sRF:
         (?:
             (?P<rf_correction_manual>[+-][\d]+)
             (?P<rf_correction_automatic>[+-][\d.]+)ppm\/
@@ -50,16 +51,17 @@ PATTERN_RECEIVER_BEACON = re.compile(r"""
         (?:\/(?P<senders_signal>[+-][\d.]+)dB@10km\[(?P<senders_messages>\d+)\])?
         (?:\/(?P<good_senders_signal>[+-][\d.]+)dB@10km\[(?P<good_senders>\d+)\/(?P<good_and_bad_senders>\d+)\])?
     )?
+    $
 """, re.VERBOSE | re.MULTILINE)
 
 PATTERN_AIRCRAFT_BEACON = re.compile(r"""
-    id(?P<details>\w{2})(?P<id>\w{6}?)
-    (?:\s(?P<climb_rate>[+-]\d+?)fpm)?
-    (?:\s(?P<turn_rate>[+-][\d.]+?)rot)?
+    id(?P<details>\w{2})(?P<id>\w{6})
+    (?:\s(?P<climb_rate>[+-]\d+)fpm)?
+    (?:\s(?P<turn_rate>[+-][\d.]+)rot)?
     (?:\sFL(?P<flight_level>[\d.]+))?
-    (?:\s(?P<signal_quality>[\d.]+?)dB)?
+    (?:\s(?P<signal_quality>[\d.]+)dB)?
     (?:\s(?P<errors>\d+)e)?
-    (?:\s(?P<frequency_offset>[+-][\d.]+?)kHz)?
+    (?:\s(?P<frequency_offset>[+-][\d.]+)kHz)?
     (?:\sgps(?P<gps_accuracy>\d+x\d+))?
     (?:\ss(?P<flarm_software_version>[\d.]+))?
     (?:\sh(?P<flarm_hardware_version>[\dA-F]{2}))?
