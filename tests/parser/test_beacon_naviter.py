d995f0320bf3844fdd71060af6e6975e29a7fa59
Konstantin Gründger <kammermark@gmx.de>
2017-09-29 09:04:09 +0200

    Cosmetics

--- tests/parser/test_beacon_naviter.py
+++ tests/parser/test_beacon_naviter.py
@@ -6,8 +6,7 @@ from ogn.parser.parse import parse_naviter_beacon
 
 class TestStringMethods(unittest.TestCase):
     def test_OGNAVI_1(self):
-        message = "id0440042121 +000fpm +0.5rot"
-        naviter_message = parse_naviter_beacon(message)
+        message = parse_naviter_beacon("id0440042121 +123fpm +0.5rot")
 
         # id0440042121 == 0b0000 0100 0100 0000 0000 0100 0010 0001 0010 0001
         # bit 0: stealth mode
@@ -16,15 +15,15 @@ class TestStringMethods(unittest.TestCase):
         # bits 6-11: address type (namespace is extended from 2 to 6 bits to avoid collisions with other tracking providers)
         # bits 12-15: reserved for use at a later time
         # bits 16-39: device id (24-bit device identifier, same as in APRS header)
-        self.assertEqual(naviter_message['stealth'], False)
-        self.assertEqual(naviter_message['do_not_track'], False)
-        self.assertEqual(naviter_message['aircraft_type'], 1)
-        self.assertEqual(naviter_message['address_type'], 4)
-        self.assertEqual(naviter_message['reserved'], 0)
-        self.assertEqual(naviter_message['address'], "042121")
+        self.assertEqual(message['stealth'], False)
+        self.assertEqual(message['do_not_track'], False)
+        self.assertEqual(message['aircraft_type'], 1)
+        self.assertEqual(message['address_type'], 4)
+        self.assertEqual(message['reserved'], 0)
+        self.assertEqual(message['address'], "042121")
 
-        self.assertAlmostEqual(naviter_message['climb_rate'] * ms2fpm, 0, 2)
-        self.assertEqual(naviter_message['turn_rate'], 0.5)
+        self.assertAlmostEqual(message['climb_rate'] * ms2fpm, 123, 2)
+        self.assertEqual(message['turn_rate'], 0.5)
 
 
 if __name__ == '__main__':
