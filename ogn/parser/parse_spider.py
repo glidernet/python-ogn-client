2c95b19aa15de3ca5ae986cd8f964e32a93b5f12
Angel Casado <acasadoalonso@gmail.com>
2017-10-16 13:53:43 +0200

    add support for skylines, spot, spider and lt24

--- ogn/parser/parse_spider.py
+++ ogn/parser/parse_spider.py
@@ -1,3 +1,8 @@
+mport re
+
+from ogn.parser.utils import fpm2ms
+from ogn.parser.pattern import PATTERN_NAVITER_BEACON
+
 def parse(aprs_comment):
     ac_match = re.search(PATTERN_AIRCRAFT_BEACON, aprs_comment)
     if ac_match:
