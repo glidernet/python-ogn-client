b48ee35e70e4b9ef15be6f40aaa3ef02033fbe0d
Zoran Bošnjak <zoran.bosnjak@via.si>
2019-12-20 18:57:36 +0100

    fix flake8 errors

--- ogn/client/__init__.py
+++ ogn/client/__init__.py
@@ -1,9 +1,10 @@
 from ogn.client.client import AprsClient    # noqa: F401
 from ogn.client.client import TelnetClient  # noqa: F401
 
+
 class CustomSettings(object):
     def __init__(self, **kw):
         self.kw = kw
+
     def __getattr__(self, name):
         return self.kw[name]
-
