7ddd0a05fa6f83daeb43db81fb8cead279831922
Angel Casado <acasadoalonso@gmail.com>
2020-08-31 12:13:50 +0200

    Get tha aircraft type in name from the APRS symbols

--- ogn/parser/utils.py
+++ ogn/parser/utils.py
@@ -46,3 +46,50 @@ def createTimestamp(time_string, reference_timestamp=None):
             result += timedelta(days=1)
 
     return result
+
+aprssymtypes=[ 
+	"/z", 			# 0 = ?
+	"/'", 			# 1 = (moto-)glider (most frequent)
+	"/'", 			# 2 = tow plane (often)
+	"/X", 			# 3 = helicopter (often)
+	"/g", 			# 4 = parachute (rare but seen - often mixed with drop plane)
+	"\\^", 			# 5 = drop plane (seen)
+	"/g", 			# 6 = hang-glider (rare but seen)
+	"/g", 			# 7 = para-glider (rare but seen)
+	"\\^",	 		# 8 = powered aircraft (often)
+	"/^", 			# 9 = jet aircraft (rare but seen)
+	"/z", 			# A = UFO (people set for fun)
+	"/O", 			# B = balloon (seen once)
+	"/O", 			# C = airship (seen once)
+	"/'", 			# D = UAV (drones, can become very common)
+	"/z", 			# E = ground support (ground vehicles at airfields)
+	"\\n" 			# F = static object (ground relay ?)
+        ]
+aprstypes=[
+	"Unkown", 		# 0 = ?
+	"Glider", 		# 1 = (moto-)glider (most frequent)
+	"Plane", 		# 2 = tow plane (often)
+	"Helicopter", 		# 3 = helicopter (often)
+	"Parachute", 		# 4 = parachute (rare but seen - often mixed with drop plane)
+	"DropPlane", 		# 5 = drop plane (seen)
+	"HangGlider", 		# 6 = hang-glider (rare but seen)
+	"ParaGlider", 		# 7 = para-glider (rare but seen)
+	"PowerAircraft",	# 8 = powered aircraft (often)
+	"Jet",	 		# 9 = jet aircraft (rare but seen)
+	"UFO", 			# A = UFO (people set for fun)
+	"Balloon", 		# B = balloon (seen once)
+	"Airship", 		# C = airship (seen once)
+	"Drone", 		# D = UAV (drones, can become very common)
+	"GroundVehicle",	# E = ground support (ground vehicles at airfields)
+	"GroundStation"		# F = static object (ground relay ?)
+        ]
+def get_aircraft_type(sym1, sym2):	# return the aircraft type based on the symbol table
+
+    sym=sym1+sym2
+    idx=0
+    while idx < 16:
+          if sym == aprssymtypes[idx]:
+             return (aprstypes[idx])
+          idx += 1
+    return ("UNKOWN")
+
