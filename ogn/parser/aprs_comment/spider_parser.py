ffa6c8b1f04f646e0389fa4aef020992dfb89148
Konstantin Gründger <kammermark@gmx.de>
2020-10-04 12:25:02 +0200

    Refactoring

--- ogn/parser/aprs_comment/spider_parser.py
+++ ogn/parser/aprs_comment/spider_parser.py
@@ -9,8 +9,8 @@ class SpiderParser(BaseParser):
         self.position_pattern = PATTERN_SPIDER_POSITION_COMMENT
 
     def parse_position(self, aprs_comment):
-        ac_match = self.position_pattern.match(aprs_comment)
-        return {'spider_id': ac_match.group('spider_id'),
-                'signal_power': int(ac_match.group('signal_power')) if ac_match.group('signal_power') else None,
-                'spider_registration': ac_match.group('spider_registration') if ac_match.group('spider_registration') else None,
-                'gps_quality': ac_match.group('gps_quality') if ac_match.group('gps_quality') else None}
+        match = self.position_pattern.match(aprs_comment)
+        return {'spider_id': match.group('spider_id'),
+                'signal_power': int(match.group('signal_power')) if match.group('signal_power') else None,
+                'spider_registration': match.group('spider_registration') if match.group('spider_registration') else None,
+                'gps_quality': match.group('gps_quality') if match.group('gps_quality') else None}
