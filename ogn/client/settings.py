c5450eb29cfc6d1bc45cf46d4e7e9a64adc167fd
Konstantin Gründger <kammermark@gmx.de>
2025-04-18 01:44:30 +0200

    Fix: get version information from pyproject.toml

--- ogn/client/settings.py
+++ ogn/client/settings.py
@@ -1,10 +1,16 @@
+import importlib.metadata
+
 APRS_SERVER_HOST = 'aprs.glidernet.org'
 APRS_SERVER_PORT_FULL_FEED = 10152
 APRS_SERVER_PORT_CLIENT_DEFINED_FILTERS = 14580
 
 APRS_APP_NAME = 'python-ogn-client'
 
-PACKAGE_VERSION = '1.2.1'
+try:
+    PACKAGE_VERSION = importlib.metadata.version(APRS_APP_NAME)
+except importlib.metadata.PackageNotFoundError:
+    PACKAGE_VERSION = '0.0.0'
+
 APRS_APP_VER = PACKAGE_VERSION[:3]
 
 APRS_KEEPALIVE_TIME = 240
