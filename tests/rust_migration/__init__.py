a25021ee9405cf7b3054df68490f41ef9ab5af3e
Konstantin Gründger <kammermark@gmx.de>
2025-05-20 19:35:50 +0200

    updated migration documentation

--- tests/rust_migration/__init__.py
+++ tests/rust_migration/__init__.py
@@ -1,11 +1,12 @@
 import os
+from pathlib import Path
 
 
 def get_valid_beacons():
     # iterate over all txt files in the valid_messages directory
     valid_beacons = []
-    for filename in os.listdir(os.path.dirname(__file__) + '/../../ogn-aprs-protocol/valid_messages/'):
-        if filename in ('OGADSB_ADSB.txt', 'OGCAPT_Capturs.txt'):
+    for filename in os.listdir(Path(__file__).parent.parent.parent / "ogn-aprs-protocol" / "valid_messages"):
+        if filename in ('OGNINRE_InReach.txt', 'OGCAPT_Capturs.txt'):
             continue
 
         if filename.endswith('.txt'):
